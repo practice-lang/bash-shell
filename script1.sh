33ef9d1eacc7b5022c2aaec96e7d4f40ba0151e1
ttakkku <k2005118@gmail.com>
2019-05-26 13:27:14 +0900

    Add files via upload

---
+++ script1.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+clear
+echo "Welcome to Bash" 
+
+echo Our shell name is $BASH
+echo Our shell version name is $BASH_VERSION
+echo Our home directory is $HOME
+echo Our current working directory is $PWD
